5b037b7909612e822c38c824b93fbf5d87cfdb89
devforfu <developer.z@outlook.com>
2025-01-29 00:24:18 +0100

    Placeholder while migrating

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,4 @@
 import { dev } from '$app/environment';
-
 export const title = 'Blog';
 export const description = 'Make blogging cool again!';
-export const url = dev ? 'http://localhost:5173' : 'https://coolblog.sh';
+export const url = dev ? 'http://localhost:5173' : 'https://iliazaitsev.me';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,9 +9,15 @@
   import '../app.css'
 
   let { children, data } = $props();
-
+  let showPlaceholder = true;
 </script>
 
+{#if showPlaceholder}
+<article class="announcement">
+  <h1>Under development 🚧</h1>
+  <p>The blog is being migrated to a new platform. Stay tuned!</p>
+</article>
+{:else}
 <div class="layout">
   <Header />
   <main>
@@ -21,8 +27,25 @@
   </main>
   <Footer />
 </div>
+{/if}
 
 <style>
+  .announcement {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    h1 {
+      text-align: center;
+      margin: 0 auto;
+    }
+    p {
+      text-align: center;
+      font-size: 2rem;
+    }
+  }
+
   .layout {
     height: 100%;
     max-inline-size: 1440px;
